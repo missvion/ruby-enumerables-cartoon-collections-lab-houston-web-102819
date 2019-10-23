d7bc42198db838aa6504a0d786bbee80c7225bbc
Vivian Tran <viviantran94@gmail.com>
2019-10-23 06:33:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,16 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize }
 end
 
-def long_planeteer_calls(array)
-  # Your code here
+def long_planeteer_calls(array) 
+  word = 0 
+  if  array.any? {|word| word.length > 4}
+    return true
+  else 
+    return false
+  word = word + 1
+  end 
+end
+
 end
 
 def find_the_cheese# code an argument here
