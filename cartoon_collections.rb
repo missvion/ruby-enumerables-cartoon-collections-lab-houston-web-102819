453c769d06bbc281d077ce9777920abd565e664d
Vivian Tran <viviantran94@gmail.com>
2019-10-23 06:29:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,10 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+  planeteer_calls.map! {|planeteer_calls| planeteer_calls.capitalize }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
 end
 
